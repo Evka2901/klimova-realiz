49ebcf53e1a73913b966f04809d93af5bf7f8876
Evka2901 <klimovaeva777@gmail.com>
2024-10-23 17:08:03 +0300

    create start commands

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.querySelector('.love input').addEventListener('change', function() {
+    document.body.classList.toggle('dark-theme', this.checked);
+});
+
+
+let registerBtn = document.querySelector('.regestration');
+
+
+    registerBtn.addEventListener("click", function () {
+        // Переходимо на сторінку реєстрації
+        window.location.assign('reg.html');
+    })
 
